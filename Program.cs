4a8d96926c13e8b934a011614dbcfc207d10fc32
andriiholubzxc111 <162205115+andriiholubzxc111@users.noreply.github.com>
2024-10-15 22:27:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList computerCompanies = new SortedList();
+            computerCompanies.Add("1", "Lenovo");
+            computerCompanies.Add("2", "Apple");
+            computerCompanies.Add("3", "Samsung");
+            computerCompanies.Add("4", "Gigabyte");
+            computerCompanies.Add("5", "MSI");
+            computerCompanies.Add("6", "Kingston");
+            computerCompanies.Add("7", "AMD");
+            computerCompanies.Add("8", "Intel");
+
+            Console.WriteLine("Список в прямому порядку:");
+            foreach (DictionaryEntry entry in computerCompanies)
+            {
+                Console.WriteLine("{0}:{1}", entry.Key, entry.Value);
+            }
+
+            Console.WriteLine("Список в зворотньому порядку:");
+            foreach (DictionaryEntry entry in computerCompanies.Cast<DictionaryEntry>().Reverse())
+            {
+                Console.WriteLine("{0}:{1}", entry.Key, entry.Value);
+            }
+
+            computerCompanies.Clear();
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+            numbers.Enqueue(13);
+            numbers.Enqueue(7);
+            numbers.Enqueue(5);
+            numbers.Enqueue(8);
+            numbers.Enqueue(28);
+            numbers.Enqueue(21);
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+                if (number < min)
+                {
+                    min = number;
+                }
+
+            }
+            int difference = max - min;
+            Console.WriteLine("Максимальне:{0}", max);
+            Console.WriteLine("Мінімальне:{0}", min);
+            Console.WriteLine("Різниця:{0}", difference);
+        }
+    }
+}
